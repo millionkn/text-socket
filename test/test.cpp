c7f825a46491cca057fd72869b569f374abe63aa
zx w <nkighter@gmail.com>
2019-04-25 20:44:48 +0800

    base

---
+++ test/test.cpp
@@ -0,0 +1,32 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+HMODULE hModule = LoadLibraryA("text-socket.dll");
+extern "C" {
+	__declspec(dllimport) void sendTo(SOCKET* socketKey, const char* data, int size);
+	__declspec(dllimport) void getMessage(SOCKET* socketKey, char* data);
+	__declspec(dllimport) void disconnect(SOCKET* socketKey);
+	__declspec(dllimport) bool connectWith(SOCKET* socketKeyBuf, const char* ip, int port);
+	__declspec(dllimport) int peekMessageSize(SOCKET* socketKey);
+}
+int main()
+{
+	SOCKET socket = { 0 };
+	bool result = connectWith(&socket, "127.0.0.1", 8090);
+	sendTo(&socket, "测试123", 8);
+    std::cout << "Hello World!\n"; 
+	disconnect(&socket);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ text-socket/dllmain.cpp
@@ -0,0 +1,70 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+		WSADATA wsaData;
+		return !WSAStartup(MAKEWORD(2, 2), &wsaData);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+		break;
+    case DLL_PROCESS_DETACH:
+		WSACleanup();
+        break;
+    }
+    return TRUE;
+}
+
+void inline sendAll(SOCKET* socket, const char* data, int size) {
+	int offset = 0;
+	while (offset < size) {
+		offset += send(*socket, data + offset, size - offset, 0);
+	}
+}
+void inline recvAll(SOCKET* socket, char* data, int size) {
+	int offset = 0;
+	while (offset < size) {
+		offset += recv(*socket, data + offset, size - offset, MSG_WAITALL);
+	}
+}
+extern "C" {
+	__declspec(dllexport) bool connectWith(SOCKET* socketKeyBuf,char* ip,int port) {
+		SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+		sockaddr_in addr = {0};
+		addr.sin_family = PF_INET;
+		inet_pton(PF_INET,ip,&addr.sin_addr);
+		addr.sin_port = htons(port);
+		if (0== connect(sock, (SOCKADDR*)& addr, sizeof(SOCKADDR))) {
+			memcpy(socketKeyBuf, &sock, sizeof(SOCKET));
+			return true;
+		}
+		return false;
+	}
+	__declspec(dllexport) void sendTo(SOCKET* socketKey,const char* data, int size) {
+		sendAll(socketKey, (const char*)& size, sizeof(int));
+		sendAll(socketKey, data, size);
+	}
+	__declspec(dllexport) int peekMessageSize(SOCKET* socketKey) {
+		int size = 0;
+		if (sizeof(int) > recv(*socketKey, (char*)& size, sizeof(int), MSG_PEEK)) {
+			return 0;
+		}else {
+			return size;
+		}
+	}
+	__declspec(dllexport) void getMessage(SOCKET* socketKey, char* data) {
+		int size = 0;
+		recvAll(socketKey, (char*)& size, sizeof(int));
+		recvAll(socketKey, data, size);
+	}
+	_declspec(dllexport) void disconnect(SOCKET* socketKey) {
+		closesocket(*socketKey);
+	}
+}
